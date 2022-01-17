5ecbb851ad1919f2b497f3931e7921802c2265d5
Manuel <manuelbarrios7891238520@gmail.com>
2022-01-16 21:59:57 -0500

    Entrar

---
+++ index.php
@@ -0,0 +1,83 @@
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+require_once( 'lib/woocommerce-api.php' );
+
+use Automattic\WooCommerce\Client;
+
+$options = array(
+	'debug'           => true,
+	'return_as_array' => false,
+	'validate_url'    => false,
+	'timeout'         => 30,
+	'ssl_verify'      => false,
+);
+
+try {
+
+	$client = new WC_API_Client(
+    'https://www.drabbalovers.co/',
+    'ck_3e6abf70b8566f628bd50ffc70dd38779caefb00',
+    'cs_2a4c74eb3f5ee2e70f57a47d540c9d28c3f19c70',
+    $options );
+
+    $woocommerce = new Client(
+    'https://www.drabbalovers.co/', 
+    'ck_3e6abf70b8566f628bd50ffc70dd38779caefb00', 
+    'cs_2a4c74eb3f5ee2e70f57a47d540c9d28c3f19c70',
+    [
+        'version' => 'wc/v3',
+    ]);
+
+    // Inciando methos GET
+    $listaProductosXcantidad = (array) $client->products->get_count();
+	$listaProductos = (array) $client->products->get();
+
+    // Numero de productos en la Base de Datos 
+	$cantidadProductos = $listaProductosXcantidad['count']; // Variable Principal
+
+    // Numero de paginas de productos	
+    $listaProductosPaginas = (array) $listaProductos['http'];
+	$response = (array) $listaProductosPaginas['response'];
+	$headers = (array) $response['headers'];
+	$paginas = $headers['x-wc-totalpages'];
+	$paginas = (int)$paginas; // Variable Principal
+
+    // Cantidad de productos en invenrario
+    echo 'Numero de productos',$cantidadProductos;
+    echo"<br>";
+
+    for ($i = 1; $i <= $paginas; $i++) { // Vista de productos X paginas
+
+        // variables de control
+        $q = 0;
+        $objPaginas = ['page' => $i];
+        $lista = (array) $woocommerce->get('products',$objPaginas);
+
+        do { // Vista de productos
+            
+            if(empty($a = (array) $lista[$q])){
+                break;
+            }
+
+            echo 'ID:',$a['id'],' - ','SKU:',$a['sku'],' - ','CANTIDAD:',$a['stock_quantity'];
+            echo"<br>";
+            
+            $q++;
+
+        } while ($q <= 9);
+        
+    }
+
+}catch ( WC_API_Client_Exception $e ) {
+
+    echo $e->getMessage() . PHP_EOL;
+    echo $e->getCode() . PHP_EOL;
+    
+    if ( $e instanceof WC_API_Client_HTTP_Exception ) {
+    
+        print_r( $e->get_request() );
+        print_r( $e->get_response() );
+    }
+}
+?>
