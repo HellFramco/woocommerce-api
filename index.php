ae203404db7c8873774ccf3b5c7d1177820c28e5
Manuel <manuelbarrios7891238520@gmail.com>
2022-01-16 22:38:05 -0500

    Fragmentando funciones

--- index.php
+++ index.php
@@ -7,81 +7,98 @@ require_once( 'lib/woocommerce-api.php' );
 // Usar los parametros de la API de Woocommerce
 use Automattic\WooCommerce\Client;
 
-// Iniciando variables de autenticacion
-$options = array(
-	'debug'           => true,
-	'return_as_array' => false,
-	'validate_url'    => false,
-	'timeout'         => 30,
-	'ssl_verify'      => false,
-);
+class APIMetodos{
 
-try {
+    public function getStockToWoocommerceDB(){
 
-    // Autenticando 
-	$client = new WC_API_Client(
-    'https://www.drabbalovers.co/',
-    'ck_3e6abf70b8566f628bd50ffc70dd38779caefb00',
-    'cs_2a4c74eb3f5ee2e70f57a47d540c9d28c3f19c70',
-    $options );
+        // Iniciando variables de autenticacion
+        $options = array(
+            'debug'           => true,
+            'return_as_array' => false,
+            'validate_url'    => false,
+            'timeout'         => 30,
+            'ssl_verify'      => false,
+        );
 
-    $woocommerce = new Client(
-    'https://www.drabbalovers.co/', 
-    'ck_3e6abf70b8566f628bd50ffc70dd38779caefb00', 
-    'cs_2a4c74eb3f5ee2e70f57a47d540c9d28c3f19c70',
-    [
-        'version' => 'wc/v3',
-    ]);
+        try {
 
-    // Inciando methos GET
-    $listaProductosXcantidad = (array) $client->products->get_count();
-	$listaProductos = (array) $client->products->get();
+            // Autenticando 
+            $client = new WC_API_Client(
+            'https://www.drabbalovers.co/',
+            'ck_3e6abf70b8566f628bd50ffc70dd38779caefb00',
+            'cs_2a4c74eb3f5ee2e70f57a47d540c9d28c3f19c70',
+            $options );
 
-    // Numero de productos en la Base de Datos 
-	$cantidadProductos = $listaProductosXcantidad['count']; // Variable Principal
+            $woocommerce = new Client(
+            'https://www.drabbalovers.co/', 
+            'ck_3e6abf70b8566f628bd50ffc70dd38779caefb00', 
+            'cs_2a4c74eb3f5ee2e70f57a47d540c9d28c3f19c70',
+            [
+                'version' => 'wc/v3',
+            ]);
 
-    // Numero de paginas de productos	
-    $listaProductosPaginas = (array) $listaProductos['http'];
-	$response = (array) $listaProductosPaginas['response'];
-	$headers = (array) $response['headers'];
-	$paginas = $headers['x-wc-totalpages'];
-	$paginas = (int)$paginas; // Variable Principal
+            // Inciando methos GET
+            $listaProductosXcantidad = (array) $client->products->get_count();
+            $listaProductos = (array) $client->products->get();
 
-    // Cantidad de productos en invenrario
-    echo 'Numero de productos',$cantidadProductos;
-    echo"<br>";
+            // Numero de productos en la Base de Datos 
+            $cantidadProductos = $listaProductosXcantidad['count']; // Variable Principal
 
-    for ($i = 1; $i <= $paginas; $i++) { // Vista de productos X paginas
+            // Numero de paginas de productos	
+            $listaProductosPaginas = (array) $listaProductos['http'];
+            $response = (array) $listaProductosPaginas['response'];
+            $headers = (array) $response['headers'];
+            $paginas = $headers['x-wc-totalpages'];
+            $paginas = (int)$paginas; // Variable Principal
 
-        // variables de control
-        $q = 0;
-        $objPaginas = ['page' => $i];
-        $lista = (array) $woocommerce->get('products',$objPaginas);
+            // Cantidad de productos en invenrario
+            echo 'Numero de productos',$cantidadProductos;
+            echo"<br>";
 
-        do { // Vista de productos
-            
-            if(empty($a = (array) $lista[$q])){
-                break;
+            for ($i = 1; $i <= $paginas; $i++) { // Vista de productos X paginas
+
+                // variables de control
+                $q = 0;
+                $objPaginas = ['page' => $i];
+                $lista = (array) $woocommerce->get('products',$objPaginas);
+
+                do { // Vista de productos
+                    
+                    if(empty($a = (array) $lista[$q])){
+                        break;
+                    }
+
+                    echo 'ID:',$a['id'],' - ','SKU:',$a['sku'],' - ','CANTIDAD:',$a['stock_quantity'];
+                    echo"<br>";
+                    
+                    $q++;
+
+                } while ($q <= 9);
+                
             }
 
-            echo 'ID:',$a['id'],' - ','SKU:',$a['sku'],' - ','CANTIDAD:',$a['stock_quantity'];
-            echo"<br>";
+        }catch ( WC_API_Client_Exception $e ) {
+
+            echo $e->getMessage() . PHP_EOL;
+            echo $e->getCode() . PHP_EOL;
             
-            $q++;
-
-        } while ($q <= 9);
-        
+            if ( $e instanceof WC_API_Client_HTTP_Exception ) {
+            
+                print_r( $e->get_request() );
+                print_r( $e->get_response() );
+            }
+        }
     }
 
-}catch ( WC_API_Client_Exception $e ) {
+    public function exportStockToWoocommerceDB(){
 
-    echo $e->getMessage() . PHP_EOL;
-    echo $e->getCode() . PHP_EOL;
-    
-    if ( $e instanceof WC_API_Client_HTTP_Exception ) {
-    
-        print_r( $e->get_request() );
-        print_r( $e->get_response() );
     }
+
 }
+
+$a = new APIMetodos();
+$a->getStockToWoocommerceDB();
+
+
+
 ?>
