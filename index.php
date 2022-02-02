aceb1d096ed4d891c5458f9a85f5fea6144ca375
Sistemas Manuel <manuelbarrios7891238520@gmail.com>
2022-02-02 14:22:20 -0500

    Funcion getOrdersFromWoocommerce funcional

--- index.php
+++ index.php
@@ -109,80 +109,239 @@ class APIMetodos{ //Creando clase de metodos
 
         try {
 
-        // Accediendo API Woocommerce
-            $client = new WC_API_Client(
-                'https://www.drabbalovers.co/',
-                'ck_7f574edb1041d0e71eadd69bb5092ee5c25dc5ca',
-                'cs_90e26e975b984c87d39cf31dbd3b913584be0557',
-                $options );
-            
-            $woocommerce = new Client(
-                'https://www.drabbalovers.co/', 
-                'ck_7f574edb1041d0e71eadd69bb5092ee5c25dc5ca', 
-                'cs_90e26e975b984c87d39cf31dbd3b913584be0557',
-            [
-                'version' => 'wc/v3',
-            ]);
-
+            // Accediendo API Woocommerce
+                $client = new WC_API_Client(
+                    'https://www.drabbalovers.co/',
+                    'ck_7f574edb1041d0e71eadd69bb5092ee5c25dc5ca',
+                    'cs_90e26e975b984c87d39cf31dbd3b913584be0557',
+                    $options );
+                
+                $woocommerce = new Client(
+                    'https://www.drabbalovers.co/', 
+                    'ck_7f574edb1041d0e71eadd69bb5092ee5c25dc5ca', 
+                    'cs_90e26e975b984c87d39cf31dbd3b913584be0557',
+                [
+                    'version' => 'wc/v3',
+                ]);
             
             // Accediendo Base de datos S-HTEX
-            $con=conectar();
+                $con=conectar();
             
-            $pedidosWoocommerce = (array) $woocommerce->get('orders');
+            // Importando datos Woocommerce
+                $pedidosWoocommerce = (array) $woocommerce->get('orders');
 
-            $sql1 = "SELECT * FROM pedidos_tienda";
-            $query1 =mysqli_query($con,$sql1);
+            // Importando datos de HTEX
+                $sql1 = "SELECT * FROM pedidos_tienda";
+                $query1 =mysqli_query($con,$sql1);
 
-            // Numero Referencias del stock de S-HTEX DB
-            if ($result=mysqli_query($con,$sql1)) {
+            // Numero pedidos en S-HTEX DB
+                if ($result=mysqli_query($con,$sql1)) {
 
-                // Numero de filas de S-HTEXDB
-                $rowcount=mysqli_num_rows($result);
-                echo 'Numero de productos en SHTEX: ', $rowcount; 
-                echo"<br>";
+                    // Numero de filas de S-HTEXDB
+                    $rowcount=mysqli_num_rows($result);
+                    echo 'Numero de productos en SHTEX: ', $rowcount; 
+                    echo"<br>";
 
+                    
+                    // Si la lista esta vacia inicializa la lista
+                    if($rowcount == 0){
+                        $primaryDBArray[] =NULL;
+                    }
                 
-                // Si algun campo esta vacio rompe el ciclo
-                if($rowcount == 0){
-                    echo 'sadsd';
-                    $primaryDBArray[] =NULL;
                 }
+
+            // Lista pedidos HTEX
+                while ($stockPrimaryDB =mysqli_fetch_array($query1)){          // Vista de pedidos S_HTEX DB
+
+                    // Filtracion de variables
+                    $idPedidoDB = $stockPrimaryDB['id_pedido'];
+                    $numeroPedidoDB = $stockPrimaryDB['numero_venta_w'];
+                    $cuponPedido = $stockPrimaryDB['cupon'];
+                    $nombreInfluenser = $stockPrimaryDB['nombre_influencer'];
+                    $fechaPedido = $stockPrimaryDB['fecha_hora'];
+                    $nombreCliente = $stockPrimaryDB['nombre_cliente'];
+                    $documentoCliente = $stockPrimaryDB['documento_cliente'];
+                    $direccionCliente = $stockPrimaryDB['direccion_cliente'];
+                    $totalPedido = $stockPrimaryDB['total_venta'];
+
+                    // Generando matriz de pedidos
+                    $primaryDBArray[] = array("$idPedidoDB", "$numeroPedidoDB","$cuponPedido","$nombreInfluenser","$fechaPedido","$nombreCliente","$documentoCliente","$direccionCliente","$totalPedido");
+
+                }
+
+            $j = 1;                                                            // Variable de control paginas de pedidos Woocommerce
+            //Lista pedidos Woocommerce
+                for($i = 0; $i < $j; $i++){                                    // Vista pedidos X paginas Woocommerce
+
+                    // Validar lista de pedidos vacia
+                    if(empty($pedidosWoocommerce)){
+                        break;
+                    }
+
+                    $q = 0;                                                                             // Variable de control pedidos X paginas
+                    $objPaginas = ['page' => $j];                                                       // Numero de paginas disponibles
+                    $pedidosWoocommerce = (array) $woocommerce->get('orders',$objPaginas);              // Lista completa pedidos Woocommerce
+
+                    while($q <= 9){
+
+                        // Si algun campo esta vacio rompe el ciclo
+                        if(empty($pedidosWoocommerce[$q])){
+                            break;
+                        }
+
+                        // Filtracion de variables
+                        $numeroPedidoWoocommerce = $pedidosWoocommerce[$q]->number;
+                        $cuponWoocommerce = $pedidosWoocommerce[$q]->coupon_lines[0]->code;
+                        $nombreInfluenser = $pedidosWoocommerce[$q]->coupon_lines[0]->meta_data[0]->value->description;
+                        $fechaPedidoWoocommerce = $pedidosWoocommerce[$q]->date_created;
+                        $nombreClienteWoocommerce = $pedidosWoocommerce[$q]->billing->first_name.' '.$pedidosWoocommerce[$q]->billing->last_name;
+                        $documentoClienteWoocommerce = $pedidosWoocommerce[$q]->meta_data[0]->value;
+                        $direccionClienteWoocommerce = $pedidosWoocommerce[$q]->billing->address_1;
+                        $totalVentaPedidoWoocommerce = $pedidosWoocommerce[$q]->total;
+                        $prc = 1;
+
+                        for($pr = 0; $pr < $prc; $pr++){
+
+                            if(empty($pedidosWoocommerce[$q]->line_items[$pr])){ break;}
+
+                            $nombreProducto = $pedidosWoocommerce[$q]->line_items[$pr]->name;
+                            $cantidadAdquirida = $pedidosWoocommerce[$q]->line_items[$pr]->quantity;
+                            $tallaProductoW = $pedidosWoocommerce[$q]->line_items[$pr]->meta_data[0]->value;
+                            $SKUTallaProductoW = $pedidosWoocommerce[$q]->line_items[$pr]->sku;
+
+                            $referenciasXPedido[] = array ("$nombreProducto","$cantidadAdquirida","$tallaProductoW","$SKUTallaProductoW");
+
+                            $prc++;
+                        }
+
+                        // Controlando datos nulos
+                        if(empty($cuponWoocommerce)){
+                            $cuponWoocommerce = 'Ninguno';
+                            $nombreInfluenser = 'Ninguno';
+                        }
+
+                        // Generando matriz de pedidos
+                        if($pedidosWoocommerce[$q]->status == 'completed'){
+
+                            $listaPedidosWoocommerce[] = array ("$numeroPedidoWoocommerce", "$cuponWoocommerce", "$nombreInfluenser", "$fechaPedidoWoocommerce", "$nombreClienteWoocommerce", "$documentoClienteWoocommerce", "$direccionClienteWoocommerce", "$totalVentaPedidoWoocommerce", $referenciasXPedido);
+                            unset($referenciasXPedido);
+                        }
+                        $q++;
+                    }
+
+                    $j++;
+                }
+
+            //Control de variables internas
+
+                //print_r($primaryDBArray);                                // Lista pedidos de HTEX
+                //print_r($listaPedidosWoocommerce);                       // Lista pedidos Woocommerce
+                //print_r($woocommerce->get('orders'));                    // Lista en bruto de pedidos Woocommerce
+                //return;
             
-            }
-            // Lista pedidos Woocommerce
-            while ($stockPrimaryDB =mysqli_fetch_array($query1)){          // Vista de productos S_HTEX DB
+            $w = 1;
+            $ww = 1;
+            $vuletaCheck = 0;
+            $pedidoEncontrado = 0;
+            // Iniciando comprobaciones de pedidos
+            for($p = 0; $p < $w ; $p++){
 
-                // Filtracion de variables
-                $idPedidoDB = $stockPrimaryDB['id_pedido'];
-                $numeroPedidoDB = $stockPrimaryDB['numero_venta_w'];
-                $cuponPedido = $stockPrimaryDB['cupon'];
-                $nombreInfluenser = $stockPrimaryDB['nombre_influencer'];
-                $fechaPedido = $stockPrimaryDB['fecha_hora'];
-                $nombreCliente = $stockPrimaryDB['nombre_cliente'];
-                $documentoCliente = $stockPrimaryDB['documento_cliente'];
-                $direccionCliente = $stockPrimaryDB['direccion_cliente'];
-                $totalPedido = $stockPrimaryDB['total_venta'];
+                if($pedidoEncontrado == 0){
 
-                // Generando matriz de productos
-                $primaryDBArray[] = array("$idPedidoDB", "$numeroPedidoDB","$cuponPedido","$nombreInfluenser","$fechaPedido","$nombreCliente","$documentoCliente","$direccionCliente","$totalPedido");
+                    if($vuletaCheck == 1){
+                        
+                        $sqli="INSERT INTO pedidos_tienda (id_pedido, numero_venta_w, cupon, nombre_influencer, fecha_hora, nombre_cliente, documento_cliente,direccion_cliente, total_venta, estado) VALUES (NULL, '$numeroPW', '$cuponW', '$nombreInfluenserW', '$fechaPedidoW', '$nombreClienteW', '$documentoClienteW', '$direccionClienteW', '$totalPedidoW', 'Pendiente por despachar');";
+                        $queryi= mysqli_query($con,$sqli);
 
-            }
+                        if($queryi){
 
-            //print_r($pedidosWoocommerce);
+                            echo"El pedido se ha actualizado en la base de datos";
+                                
+                        }else {
 
-            $j = 1;
-            //Lista pedidos HTEX
-            for($i = 0; $i < $j; $i++){
+                        }
 
-                $q = 0;
-                $objPaginas = ['page' => $i];                                 // Numero de paginas disponibles
-                $lista = (array) $woocommerce->get('products',$objPaginas);   // Lista completa STOCK Woocommerce
+                        $r = 1;
+                        for($iii = 0; $iii < $r; $iii++){
+
+                            if(empty($referenciasPW[$iii])){ break;}
+
+                            $SKURPW = $referenciasPW[$iii][3];
+                            $tallaRPW = $referenciasPW[$iii][2];
+                            $cantidadRPW = $referenciasPW[$iii][1];
+
+                            print_r($SKURPW);
+                            echo "<br>";
+                            print_r($tallaRPW);
+                            echo "<br>";
+                            print_r($cantidadRPW);
+                            echo "<br>";
+
+                            $sqli="INSERT INTO lista_productos_tienda (id_lista, numero_venta_w, referencia, talla, cantidad) VALUES (NULL, '$numeroPW', '$SKURPW', '$tallaRPW', '$cantidadRPW');";
+                            $queryi= mysqli_query($con,$sqli);
+
+                            if($queryi){
+
+                                echo"Las referencias fueron anexadas";
+                                echo "<br>";
+
+                            }else {
+
+                            }
+
+
+                            $r++;
+                        }
+
+                    }
+
+                }
+
+                if(empty($listaPedidosWoocommerce[$p])){
+                    break;
+                }
+
+                $numeroPW = $listaPedidosWoocommerce[$p][0];
+                $cuponW = $listaPedidosWoocommerce[$p][1];
+                $nombreInfluenserW = $listaPedidosWoocommerce[$p][2];
+                $fechaPedidoW = $listaPedidosWoocommerce[$p][3];
+                $nombreClienteW = $listaPedidosWoocommerce[$p][4];
+                $documentoClienteW = $listaPedidosWoocommerce[$p][5];
+                $direccionClienteW = $listaPedidosWoocommerce[$p][6];
+                $totalPedidoW = $listaPedidosWoocommerce[$p][7];
+                $referenciasPW = $listaPedidosWoocommerce[$p][8];
+                $vuletaCheck = 0;
 
                 
+                print_r($referenciasPW);
 
-                
+                for($pp = 0; $pp < $ww; $pp++){
 
+                    if(empty($primaryDBArray[$pp][1])){ break;}
+
+                    if($numeroPW == $primaryDBArray[$pp][1]){
+
+                        $pedidoEncontrado = 1;
+                        echo 'El pedido '.$numeroPW.' ya existe en HTEX';
+                        echo "<br>";
+                        break;
+
+                    }else{
+
+                        echo 'Buscando';
+                        echo "<br>";
+
+                    }
+
+                    $ww++;
+                }
+
+                $vuletaCheck = 1;
+                $w++;
             }
+
+
+
 
         }catch ( WC_API_Client_Exception $e ) {
 
